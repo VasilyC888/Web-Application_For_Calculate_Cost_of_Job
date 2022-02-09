bab49e0de66e3c2a09306daa1d34919f0be7002e
VasilyC888 <kasoi129@gmail.com>
2022-02-09 23:52:03 +0200

    add files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const squareInput = document.querySelector('#square-input')
+const squareRange = document.querySelector('#square-range')
+
+
+squareRange.addEventListener('input', function() {
+    
+})
