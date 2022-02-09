aebb776134649c480d09c4eb850966f048688b0c
VasilyC888 <kasoi129@gmail.com>
2022-02-10 00:25:26 +0200

    comm

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 const squareInput = document.querySelector('#square-input')
 const squareRange = document.querySelector('#square-range')
 
+const basePrise = 6000
+
 
 squareRange.addEventListener('input', function() {
-    
+    squareInput.value = squareRange.value
 })
+
+
+squareInput.addEventListener('input', function() {
+    squareRange.value = squareInput.value
+})
+
+const totalPrice = basePrise * parseInt(squareInput.value)
