2c15df57dcd3659e7517585dc0d2abe17a72cd29
VasilyC888 <kasoi129@gmail.com>
2022-02-10 12:54:30 +0200

    comm

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ const radioType = document.querySelectorAll('input[name="type"]')
 const radioBuilding = document.querySelectorAll('input[name="building"]')
 const radioRooms = document.querySelectorAll('input[name="rooms"]')
 
+const ceilings = document.querySelector('input[name="ceiling"]')
+const walls = document.querySelector('input[name="walls"]')
+const floors = document.querySelector('input[name="floor"]')
+
 const basePrise = 6000
 
 const totalPriceElement = document.querySelector('#total-price')
@@ -40,6 +44,18 @@ function calculate() {
         if (radio.checked === true) {
             totalPrice *= parseFloat(radio.value)
         }
+    }
+
+    if (ceilings.checked) {
+        totalPrice *= parseFloat(ceilings.value)
+    }
+
+    if (walls.checked) {
+        totalPrice *= parseFloat(walls.value)
+    }
+
+    if (floors.checked) {
+        totalPrice *= parseFloat(floors.value)
     }
 
     const formatter = new Intl.NumberFormat('ru')
